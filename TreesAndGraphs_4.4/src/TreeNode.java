f3bb97cc1e5c663080c7e3f9775b36b8fabb457d
denerhas <45282777+denerhas@users.noreply.github.com>
2019-01-19 22:00:13 +0300

    Add files via upload
    Solution that check if a tree is balanced or not

---
+++ TreesAndGraphs_4.4/src/TreeNode.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author Hasan
+ */
+public class TreeNode {
+    int data;
+    public TreeNode left;
+    public TreeNode right;
+    
+    public TreeNode(int data)
+    {
+        this.data = data;
+    }
+    
+    public TreeNode(int data, TreeNode left, TreeNode right)
+    {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
