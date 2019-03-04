28b314b1eb65f4a09beeb5a838fcb82ce28a5934
denerhas <45282777+denerhas@users.noreply.github.com>
2019-03-04 14:41:35 +0300

    Add files via upload
    Magic Index application has been added

---
+++ RecursionAndDynamics_8.3/src/Program.java
@@ -0,0 +1,31 @@
+
+public class Program {
+
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public static int magicFast(int[] array)
+	{
+		return magicFast(array, 0, array.length - 1);
+	}
+	
+	public static int magicFast(int[] array, int start, int end)
+	{
+		if(end < start)
+		{
+			return -1;
+		}
+		
+		int mid = (start + end) / 2;
+		
+		if(array[mid] == mid) {
+			return mid;
+		}else if(array[mid] > mid) {
+			return magicFast(array, start, mid -1);
+		}else {
+			return magicFast(array, mid+1, end);
+		}
+	}
+}
