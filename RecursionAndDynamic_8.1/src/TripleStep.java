fefadc52e91c0bdb345a0ecb8816cd06410e8842
denerhas <45282777+denerhas@users.noreply.github.com>
2019-03-04 10:56:42 +0300

    Add files via upload
    A child running up a staircase with n steps and can hop either 1 step, 2 steps or 3 steps

---
+++ RecursionAndDynamic_8.1/src/TripleStep.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class TripleStep {
+
+	public static void main(String[] args)
+	{
+		System.out.println(countWays(10));
+		return;
+	}
+	
+	static int countWays(int n)
+	{
+		if(n < 0)
+			return 0;
+		if(n == 0)
+			return 1;
+		
+		return countWays(n-3) + countWays(n-2) + countWays(n-1);
+	}
+	
+	
+	static int countWaysWithMemoization(int n)
+	{
+		int[] memo = new int[n+1];
+		Arrays.fill(memo, -1);
+		return countWays(n, memo);
+	}
+	
+	static int countWays(int n, int[] memo)
+	{
+		if(n < 0)
+			return 0;
+		else if(n == 0)
+			return 1;
+		else if(memo[n] > -1)
+			return memo[n];
+		else
+			memo[n] = countWays(n-1, memo) + countWays(n-1, memo) + countWays(n-3, memo);
+		
+		return memo[n];
+	}
+}
